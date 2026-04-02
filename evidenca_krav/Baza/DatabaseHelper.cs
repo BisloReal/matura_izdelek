1a6af7738b2526ac0e3e154ae52fb05312ff7049
BisloReal <123933804+BisloReal@users.noreply.github.com>
2026-04-02 12:11:05 +0200

    Baza
    Povezava uspostavljena, ustvarjen je bil tudi dastabasehelper class z funkcijama za odpiranje in zapiranje povezave

---
+++ evidenca_krav/Baza/DatabaseHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace evidenca_krav
+{
+    public class DatabaseHelper
+    {
+        private SQLiteConnection _connection;
+
+        public DatabaseHelper()
+        {
+            _connection = new SQLiteConnection("Data Source=evidenca_krav.db");
+        }
+
+        public void Odpri()
+        {
+            try
+            {
+                if (_connection.State != ConnectionState.Open)
+                {
+                    _connection.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Povezava do baze ni bila uspešna: " + ex.Message);
+            }
+        }
+
+        public void Zapri()
+        {
+            try
+            {
+                if (_connection.State != ConnectionState.Closed)
+                {
+                    _connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Zaprejte povezave do baze ni bilo uspešno: " + ex.Message);
+            }
+        }
+    }
+}
