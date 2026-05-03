c80d84cda804ec0c5f161ac1092c22488897a593
BisloReal <123933804+BisloReal@users.noreply.github.com>
2026-05-03 11:22:55 +0200

    Krave
    filtriranje prikazov

--- evidenca_krav/NavigationBarUserControls/Krave.Designer.cs
+++ evidenca_krav/NavigationBarUserControls/Krave.Designer.cs
@@ -35,6 +35,8 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -77,6 +79,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(193, 30);
             this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // contextMenuStrip1
             // 
@@ -89,15 +92,48 @@
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(347, 82);
+            this.comboBox1.Items.AddRange(new object[] {
+            "Brez filtra",
+            "Ime",
+            "Ušesna številka",
+            "Pasma",
+            "Ime mame",
+            "Ime očeta",
+            "Datum rojstva"});
+            this.comboBox1.Location = new System.Drawing.Point(280, 82);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 33);
+            this.comboBox1.Size = new System.Drawing.Size(188, 33);
             this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(209, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Išči po:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(474, 82);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(193, 30);
+            this.dateTimePicker1.TabIndex = 9;
+            this.dateTimePicker1.Visible = false;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // Krave
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.flowLayoutPanel1);
@@ -118,5 +154,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
--- evidenca_krav/NavigationBarUserControls/Krave.cs
+++ evidenca_krav/NavigationBarUserControls/Krave.cs
@@ -17,6 +17,7 @@ namespace evidenca_krav.NavigationBar
     public partial class Krave : UserControl, IRefreshable
     {
         private DatabaseHelper db;
+        private List<KraveRazred> vseKrave = new List<KraveRazred>();
 
         public Krave(DatabaseHelper dbHelper)
         {
@@ -24,13 +25,18 @@ namespace evidenca_krav.NavigationBar
             db = dbHelper;
 
             naloziKrave();
+            comboBox1.SelectedIndex = 0;
         }
-
 
         private void naloziKrave()
         {
+            vseKrave = db.PridobiKrave();
+            prikaziKrave(vseKrave);
+        }
+
+        private void prikaziKrave(List<KraveRazred> krave)
+        {
             flowLayoutPanel1.Controls.Clear();
-            List<KraveRazred> krave = db.PridobiKrave();
             foreach (KraveRazred k in krave)
             {
                 flowLayoutPanel1.Controls.Add(new KravaCard(db, k));
@@ -40,6 +46,103 @@ namespace evidenca_krav.NavigationBar
         public void Posodobi()
         {
             naloziKrave();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            filtriraj();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            if (comboBox1.SelectedItem.ToString() == "Datum rojstva")
+            {
+                textBox1.Visible = false;
+                dateTimePicker1.Visible = true;
+            }
+            else
+            {
+                textBox1.Visible = true;
+                dateTimePicker1.Visible = false;
+            }
+
+            filtriraj();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            filtriraj();
+        }
+
+        private void filtriraj()
+        {
+            string kriterij = comboBox1.SelectedItem.ToString();
+
+            if (kriterij == "Brez filtra")
+            {
+                textBox1.Visible = true;
+                dateTimePicker1.Visible = false;
+                prikaziKrave(vseKrave);
+                return;
+            }
+
+            List<KraveRazred> filtrirane = new List<KraveRazred>();
+
+            foreach (KraveRazred k in vseKrave)
+            {
+                if (kriterij == "Ime")
+                {
+                    if (k.Ime != null && k.Ime.ToLower().Contains(textBox1.Text.Trim().ToLower()))
+                    {
+                        filtrirane.Add(k);
+                    }
+                }
+                else if (kriterij == "Ušesna številka")
+                {
+                    if (k.UsesnaSt != null && k.UsesnaSt.ToLower().Contains(textBox1.Text.Trim().ToLower()))
+                    {
+                        filtrirane.Add(k);
+                    }
+                }
+                else if (kriterij == "Pasma")
+                {
+                    if (k.Pasma != null && k.Pasma.ToLower().Contains(textBox1.Text.Trim().ToLower()))
+                    {
+                        filtrirane.Add(k);
+                    }
+                }
+                else if (kriterij == "Ime mame")
+                {
+                    if (k.ImeMame != null && k.ImeMame.ToLower().Contains(textBox1.Text.Trim().ToLower()))
+                    {
+                        filtrirane.Add(k);
+                    }
+                }
+                else if (kriterij == "Ime očeta")
+                {
+                    if (k.ImeOceta != null && k.ImeOceta.ToLower().Contains(textBox1.Text.Trim().ToLower()))
+                    {
+                        filtrirane.Add(k);
+                    }
+                }
+                else if (kriterij == "Datum rojstva")
+                {
+                    if (k.DatumRoj.Date == dateTimePicker1.Value.Date)
+                    {
+                        filtrirane.Add(k);
+                    }
+                }
+            }
+
+            if (kriterij != "Datum rojstva" && string.IsNullOrEmpty(textBox1.Text.Trim()))
+            {
+                prikaziKrave(vseKrave);
+            }
+            else
+            {
+                prikaziKrave(filtrirane);
+            }
         }
     }
 }
